a2628575b6eca777cebbf1a84b865d1f97532859
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-05-25 15:16:17 -0300

    Resolvendo bugs

---
+++ EstoqueLoja.WPF/Views/LoginWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Windows;
+
+namespace EstoqueLoja.WPF.Views
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Botão Funcionando");
+        }
+    }
+}
