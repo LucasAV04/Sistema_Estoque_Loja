ea5e430f59b86ca4d1fd1febd682d216b19bef75
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-05-29 17:18:10 -0300

    Adicionado mudanças na tela principal e Adicionado a tela Produtos

--- EstoqueLoja.WPF/Views/MainWindow.xaml.cs
+++ EstoqueLoja.WPF/Views/MainWindow.xaml.cs
@@ -9,47 +9,40 @@ namespace EstoqueLoja.WPF.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            TxtUsuarioLogado.Text = $"{SessaoUsuario.Usuario} ({SessaoUsuario.Role})";
+            TxtUsuarioLogado.Text = SessaoUsuario.Usuario;
+            TxtRoleLogada.Text = SessaoUsuario.Role;
         }
 
-        private void TrocarConteudo(string titulo, string mensagem)
+        private void TrocarConteudo(Page pagina)
         {
-            TxtTituloPagina.Text = titulo;
-
-            ConteudoPrincipal.Children.Clear();
-
-            ConteudoPrincipal.Children.Add(new TextBlock
-            {
-                Text = mensagem,
-                FontSize = 24,
-                Foreground = System.Windows.Media.Brushes.DimGray,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center
-            });
+            FramePrincipal.Navigate(pagina);
         }
 
         private void BtnProdutos_Click(object sender, RoutedEventArgs e)
         {
-            TrocarConteudo("Produtos", "Tela de Produtos será carregada aqui.");
+            TrocarConteudo(new ProdutosPage());
         }
 
         private void BtnEstoque_Click(object sender, RoutedEventArgs e)
         {
-            TrocarConteudo("Estoque", "Tela de Estoque será carregada aqui.");
+
+            FramePrincipal.Navigate(new ProdutosPage());
         }
 
         private void BtnEntrada_Click(object sender, RoutedEventArgs e)
         {
-            TrocarConteudo("Entrada de Estoque", "Tela de Entrada será Carregada aqui");
+
+            FramePrincipal.Navigate(new ProdutosPage());
         }
         private void BtnSaida_Click(object sender, RoutedEventArgs e)
         {
-            TrocarConteudo("Saída / Venda", "Tela de saída será carregada aqui.");
+
+            FramePrincipal.Navigate(new ProdutosPage());
         }
         private void BtnHistorico_Click(object sender, RoutedEventArgs e)
         {
-            TrocarConteudo("Histórico", "Tela de histórico será carregada aqui.");
+
+            FramePrincipal.Navigate(new ProdutosPage());
         }
 
         private void BtnSair_Click(object sender, RoutedEventArgs e)
---
+++ EstoqueLoja.WPF/Views/ProdutosPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EstoqueLoja.WPF.Views
+{
+    public partial class ProdutosPage: Page
+    {
+        public ProdutosPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Buscar produtos funcionando!");
+        }
+    }
+}
