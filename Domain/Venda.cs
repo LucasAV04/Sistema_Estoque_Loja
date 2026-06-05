ff83bb7cb3bab4e7d8caae5398fbf9b599c16bd6
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-06-05 12:59:29 -0300

    Adicionado novas classes de Dominio

---
+++ Domain/Venda.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public decimal ValorTotal { get; set; }
+        public List<VendaItem> Itens { get; set; } = new();
+    }
+}
---
+++ Domain/VendaItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    public class VendaItem
+    {
+        public int Id { get; set; }
+        public int VendaId { get; set; }
+        public int ProdutoId { get; set; }
+        public string NomeProduto { get; set; } = string.Empty;
+        public string RefProduto { get; set; } = string.Empty;
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal => Quantidade * ValorUnitario;
+    }
+}
